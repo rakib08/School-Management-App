e1d65954746618bae5197550ec3a095db64c3ab4
rakib08 <96006245+rakib08@users.noreply.github.com>
2021-12-12 20:22:12 +0600

    start

--- Start.java
+++ Start.java
@@ -3,4 +3,88 @@ import classes.*;
 import fileio.*;
 import java.util.Scanner;
 
-s
+
+public class Start
+{
+	public static void main(String args[])
+	{
+		scanner sc = new scanner(system.in)
+        FileReadWriteDemo frwd = new FileReadWriteDemo();
+
+        public School("Cant Public School & College","Saidpur, Nilphamari" )
+
+        system.out.println("----------------------------------------------------");
+        system.out.println("School Management System");
+        system.out.println("----------------------------------------------------\n");
+        
+        boolean repeat = true;
+        
+        while(repeat)
+        {
+            system.out.println("What do you want to do?");
+        
+            system.out.println("\t1. Student Management.");
+            system.out.println("\t2. Teacher Management.");
+            system.out.println("\t3. Employee Management.");
+            system.out.println("\t4. Show School Information.");
+            system.out.println("\t5. Exit");
+            system.out.println("");
+           
+            System.out.println("\n---------------------------------------"
+            system.out.println("Enter your choice: ");
+            int choice = sc.nextInt();
+            System.out.println("---------------------------------------\n"
+
+            switch(option1)
+					{
+						case 1:
+							
+							System.out.println("#####################################");
+							System.out.println("You have selected Insert Student Management");
+							System.out.println("#####################################");
+							
+							break;
+							
+						case 2:
+							
+							System.out.println("#####################################");
+							System.out.println("You have selected Remove Teacher Management");
+							System.out.println("#####################################");
+							break;
+							
+						case 3:
+							
+							System.out.println("#####################################");
+							System.out.println("You have selected Search Employee Management");
+							System.out.println("#####################################");
+							break;
+							
+						case 4:
+						
+							System.out.println("#####################################");
+							System.out.println("You have selected Show All School Information");
+							System.out.println("#####################################");
+							break;
+
+                        case 5:
+							
+							System.out.println("#####################################");
+							System.out.println("You have selected to Go Back....");
+							System.out.println("#####################################");
+							
+							break;
+
+                        default:
+							
+							System.out.println("#####################################");
+							System.out.println("Invalid Option... Going Back...");
+							System.out.println("#####################################");
+							break;
+                    }
+                    break;
+                                
+
+            
+        }
+
+	}
